afd816f180eab587b589fa8c02f04a82cb131aa1
kenji hajiro <mademoiselle3824@gmail.com>
2023-05-22 15:38:41 +0900

    環境構築

---
+++ main.cc
@@ -0,0 +1,16 @@
+// // #include <bits/stdc..h>
+// #include <bits/stdc++.h>
+// // #include <iostream>
+
+// int main(){
+//   std::cout << "hello world" << std::endl;
+
+//   return 0;
+// }
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main () {
+  cout << "Hello World!" << endl;
+}
